6102082573b90a772f6735f6c4034ed5b1fbabb5
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:12:50 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,7 @@
+function tugas(){
+    var data=['Saya', 'Ingin', 'Belajar', 'Bersama'];
+    data.forEach(function(item, index,array){
+        console.log("Item : ",item," index ke ",index);
+    })
+}
+tugas();
